ebfb06b96be9de13c510455b691e0baa961366a5
Sergio Gimeno <gimeno.sergio@gmail.com>
2017-05-05 14:41:11 +0200

    cleanup npm scripts

--- test/integration/run.test.js
+++ test/integration/run.test.js
@@ -1,7 +1,6 @@
 import test from 'ava'
-import starter from '../../lib'
+// import starter from '../../lib'
 // import testSpawningStarterChildProcessInCwd from '../helpers/test-spawning-starter-child-process-in-cwd'
-
 
 test.skip('Running starter server with `node app.js`', t => {
   // testSpawningStarterChildProcessInCwd()
