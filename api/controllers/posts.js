9073fb6f7b7071e47700924a3d88cbbc0d6f681e
Sergio Gimeno <sgimeno@users.noreply.github.com>
2017-05-05 15:10:14 +0200

    chore: de-couple framework logic from API applicaton (#1)
    * add basic tests and travis file
    
    * cleanup npm scripts
    
    * order sccripts alphabetically
    
    * add semantic-release npm script
    
    * run only on NodeJS 7 due to koa2 middleware restrictions

--- api/controllers/posts.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async create(ctx) {
-    ctx.body = await ctx.services.crud.create('post', {
-      authorId: 1,
-      title: 'hello',
-      body: 'world'
-    })
-  }
-}
--- config/services.js
+++
@@ -1,7 +0,0 @@
-import CrudService from 'api/services/crud'
-
-export default app => {
-  return {
-    crud: CrudService(app)
-  }
-}
